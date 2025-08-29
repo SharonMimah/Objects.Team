ab42afb5eecad3667647794388523e36d60840df
SharonMimah <ndugga02sharon@gmail.com>
2025-08-29 13:10:32 +0300

    objects

---
+++ objects.js
@@ -0,0 +1,14 @@
+const user ={
+    name: "Shannie",
+    age: 19,
+
+bio: "electrical Engineer,Graduate Member UIPE, Researcher and lover of Mathematics",
+image:"MV5BZWZkNjM0NTEtNjJlYy00Y2Y1LTk5MjgtNzhiMjAyZjkxODBjXkEyXkFqcGc@._V1_.jpg",
+
+}
+
+//use Javascript to Update the HTML with object values
+document.getElementById("user-name").textContent = user.name;
+document.getElementById("user-age").textContent= "Age: " + user.age;
+document.getElementById("user-bio").textContent = user.bio;
+document.getElementById("user-image").src = user.image;
